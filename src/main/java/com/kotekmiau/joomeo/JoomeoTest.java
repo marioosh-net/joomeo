e1ba24b0efa375eb3613a1cfff23063cc2f3f1d8
marioosh <marioosh.net@gmail.com>
2012-02-13 09:33:57 +0100

    config in file log4j

--- src/main/java/com/kotekmiau/joomeo/JoomeoTest.java
+++ src/main/java/com/kotekmiau/joomeo/JoomeoTest.java
@@ -1,10 +1,18 @@
 package com.kotekmiau.joomeo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Properties;
+import org.apache.log4j.Logger;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
@@ -15,30 +23,95 @@ import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
  */
 public class JoomeoTest {
 
-    private static String endpoint = "https://api.joomeo.com/xmlrpc.php";
+	Logger log = Logger.getLogger(getClass());
+	
+    private static String endpoint = "http://api.joomeo.com/xmlrpc.php";
+    final static String CONFIG_FILE = ".joomeo-config";
+    
+    private Properties config;
     private XmlRpcClient client;
 
     public JoomeoTest() {
-        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+		
+    	/**
+    	 * read config (to get apiKey, login, password, spacename)
+    	 */
+    	config = config();
+    	
+        XmlRpcClientConfigImpl rpcConfig = new XmlRpcClientConfigImpl();
         try {
-            config.setServerURL(new URL(endpoint));
+            rpcConfig.setServerURL(new URL(endpoint));
             client = new XmlRpcClient();
-            client.setConfig(config);
+            client.setConfig(rpcConfig);
 
-            Object[] params = null;// = new Object[]{new Integer(33), new Integer(9)};
-            System.out.println(call("joomeo.version.about", params));
-            System.out.println(call("system.listMethods", params));
-        } catch (XmlRpcException ex) {
-            Logger.getLogger(JoomeoTest.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(JoomeoTest.class.getName()).log(Level.SEVERE, null, ex);
+            /**
+             * joomeo.version.about
+             */
+            log.info(call("joomeo.version.about", null));
+            
+            /**
+             * system.listMethods
+             */
+            Object[] l = (Object[]) call("system.listMethods", null);
+            log.info(new HashSet(Arrays.asList(l)));
+            
+            /**
+             * system.listMethods
+             */
+            HashMap result = (HashMap) call("joomeo.session.init", new HashMap<String, String>() {{
+	            put("apikey", config.getProperty("apiKey"));
+	            put("spacename", config.getProperty("spacename"));
+	            put("login", config.getProperty("login"));
+	            put("password", config.getProperty("password"));
+			}});
+            log.info(result);
+            
+        } catch (XmlRpcException e) {
+        	log.error(e.getMessage(), e);
+        } catch (MalformedURLException e) {
+        	log.error(e.getMessage(), e);
         }
     }
 
-    private Object call(String method, Object[] params) throws XmlRpcException {
-        return client.execute(method, params);
+    private Object call(String method, Map<String, String> params) throws XmlRpcException {
+    	if(params != null) {
+    		Map params2[] = new Map[1];
+    		params2[0] = params;
+    		return client.execute(method, params2);
+    	}
+    	Object[] params1 = null;
+		return client.execute(method, params1);    
     }
 
+    private Properties config() {
+		FileInputStream in1 = null;
+		Properties properties = null;
+		try {
+			File f = new File(System.getProperty("user.home"), CONFIG_FILE);
+			properties = new Properties();
+			if (f.createNewFile()) {
+				properties.put("apiKey", "API KEY HERE");
+				properties.put("login", "LOGIN HERE");
+				properties.put("password", "PASSWORD HERE");
+				properties.put("spacename", "SPACENAME HERE");
+				properties.store(new FileOutputStream(f), "joomeo config");
+			}
+			in1 = new FileInputStream(f);
+			properties.load(in1);
+			return properties;
+		} catch (FileNotFoundException e) {
+			log.error(e.getMessage(), e);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		} finally {
+			try {
+				in1.close();
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+		return null;
+    }
     public static void main(String[] args) {
         new JoomeoTest();
     }
